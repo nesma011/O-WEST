a13a4f11c112805fdc3cd37a760238b836434948
nesma011 <nesmaatef349@gmail.com>
2025-04-17 22:17:23 +0200

    ok

--- src/Home.jsx
+++ src/Home.jsx
@@ -68,7 +68,7 @@ export default function Home() {
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
-            className="text-6xl md:text-6xl font-extrabold mb-12 tracking-wide"
+            className="text-3xl sm:text-4xl md:text-6xl font-extrabold mb-12 tracking-wide"
           >
             {name}
           </motion.h1>
